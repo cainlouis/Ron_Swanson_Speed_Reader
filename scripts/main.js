43a93498e73ca4053befe90e134dbb376fb85000
Nael Louis <nael.louis1002@gmail.com>
2020-11-25 15:23:45 -0500

    Functions setupWpm, speedHandler, and global variable has been added

--- scripts/main.js
+++ scripts/main.js
@@ -1,42 +1,61 @@
+//Sergio Arturo Segrera, Nael Louis
 "use strict";
 
 document.addEventListener("DOMContentLoaded", function() {
     setup();
 }, false);
 
-const APIURL = "https://ron-swanson-quotes.herokuapp.com/v2/quotes";
-let start = false;
-let btn = document.querySelector("#startstopbtn");
-let interval;
-let wpm = 60000;
-let index = 0;
+let global = {};
 
+//
 function setup() {
-     btn.addEventListener('click', startStop);
-     let savedWpm = localStorage.getItem("wpm");
-     if (savedWpm == null) {
-         savedWpm = 100;
-     }
-    document.querySelector('#wpminput').value = savedWpm;
+    global.APIURL = "https://ron-swanson-quotes.herokuapp.com/v2/quotes";
+    global.start = false;
+    global.index = 0;
+    global.wpm = 60000;
+    global.btn = document.querySelector("#startstopbtn");
+    global.input = document.querySelector('#wpminput');
+
+    setupWpm();
+    global.btn.addEventListener('click', startStop);
+    global.input.addEventListener('click', speedHandler);
+
+    /*global.savedWpm = localStorage.getItem("wpm");
+    if (global.savedWpm == null) {
+        global.savedWpm = 100;
+    }
+    global.input.value = global.savedWpm;*/
+}
+
+function setupWpm() {
+    if (localStorage.getItem("wpm") === null) {
+        localStorage.setItem("wpm", JSON.stringify(100));
+        global.savedWpm = 100;
+        global.input.value = global.savedWpm;
+    }
+    else {
+        global.savedWpm = JSON.parse(localStorage.getItem("wpm"));
+        global.input.value = global.savedWpm;
+    }
 }
 
 function startStop(e) {
-    if (!start) {
+    if (!global.start) {
         getNext()
         // TODO: Change button text to Stop
-        btn.childNodes[0].nodeValue = "Stop";
-        start = true;
+        global.btn.childNodes[0].nodeValue = "Stop";
+        global.start = true;
     } else {
         // TODO: Stop interval, change text to Start
-        btn.childNodes[0].nodeValue = "Start";
-        clearInterval(interval);
-        index = 0;
-        start = false;
+        global.btn.childNodes[0].nodeValue = "Start";
+        clearInterval(global.interval);
+        global.index = 0;
+        global.start = false;
     }
 }
 
 function getNext() {
-    fetch(APIURL)
+    fetch(global.APIURL)
         .then(response => {
             return response.json()
         })
@@ -49,23 +68,28 @@ function getNext() {
         })
 }
 
+function speedHandler(e) {
+    global.savedWpm = global.input.value;
+    window.localStorage.setItem("wpm", JSON.stringify(global.savedWpm));
+}
 function displayQuote(words) {
-    let input = document.querySelector('#wpminput').value;
-    window.localStorage.setItem("wpm", input);
-    interval = setInterval(displayWord, wpm/input, words);
+    //let input = document.querySelector('#wpminput').value;
+    //window.localStorage.setItem("wpm", input);
+    console.log(global.savedWpm);
+    global.interval = setInterval(displayWord, global.wpm/global.savedWpm, words);
 }
 
 function displayWord(words) {
-    let word = words[index];
+    let word = words[global.index];
     console.log(word);
     let isSplit = splitWord(word);
     document.querySelector('#left').textContent = isSplit[0];
     document.querySelector('#center').textContent = isSplit[1];
     document.querySelector('#right').textContent = isSplit[2];
-    index++;
-    if(words.length == index) {
-        clearInterval(interval);
-        index = 0;
+    global.index++;
+    if(words.length == global.index) {
+        clearInterval(global.interval);
+        global.index = 0;
         getNext();
     }
 }
