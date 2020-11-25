a84fd49abfead481969d7b59c68094e84aba196e
Nael Louis <nael.louis1002@gmail.com>
2020-11-24 22:03:46 -0500

    Done with displayWord. and changed the html to get rid of the space between the letters

--- scripts/main.js
+++ scripts/main.js
@@ -9,6 +9,7 @@ var start = false;
 let btn = document.querySelector("#startstopbtn");
 let interval;
 let wpm = 60000;
+let index = 0;
 
 function setup() {
      btn.addEventListener('click', startStop);
@@ -24,6 +25,7 @@ function startStop(e) {
         // TODO: Stop interval, change text to Start
         btn.childNodes[0].nodeValue = "Start";
         clearInterval(interval);
+        index = 0;
         start = false;
     }
 }
@@ -34,8 +36,8 @@ function getNext() {
             return response.json()
         })
         .then (data => {
-            let word = data[0].split(" ");
-            displayQuote(word);
+            let words = data[0].split(" ");
+            displayQuote(words);
         })
         .catch((error) => {
             console.log(error);
@@ -44,11 +46,40 @@ function getNext() {
 
 function displayQuote(words) {
     let input = document.querySelector('#wpminput').value;
-    words.forEach(element => {
-        interval = setInterval(displayWord, wpm/input);
-    });
+    interval = setInterval(displayWord, wpm/input, words);
 }
 
-function displayWord() {
+function displayWord(words) {
+    let word = words[index];
+    console.log(word);
+    let isSplit = splitWord(word);
+    document.querySelector('#left').textContent = isSplit[0];
+    document.querySelector('#center').textContent = isSplit[1];
+    document.querySelector('#right').textContent = isSplit[2];
+    index++;
+    if(words.length == index) {
+        clearInterval(interval);
+        index = 0;
+        getNext();
+    }
+}
 
+function splitWord(word) {
+    let size = word.length; 
+    if(size < 2) {
+        return ['    ', word, ''];
+    }
+    if(size == 2) {
+        return [`   ${word.substring(0, 1)}`, word.substring(1, 2), ''];
+    }
+    if(size > 2 && size < 6) {
+        return [`   ${word.substring(0, 1)}`, word.substring(1, 2), word.substring(2, size)];
+    }
+    if(size < 10){
+        return [`  ${word.substring(0, 2)}`, word.substring(2, 3), word.substring(3, size)];
+    }
+    if(size < 14) {
+        return [` ${word.substring(0, 3)}`, word.substring(3, 4), word.substring(4, size)];
+    }
+    return [word.substring(0, 4), word.substring(4, 5), word.substring(5, size)];
 }
